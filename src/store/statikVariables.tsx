cfcad72fa117b235e9bf31da8d7cae5fc18bff1a
Emre K <emre.kirbas.2018@gmail.com>
2024-03-06 20:27:14 +0300

    "deploy edit staticVarriables"

---
+++ src/store/statikVariables.tsx
@@ -0,0 +1,11 @@
+const statikVariables = {
+  imageBaseUrl: "https://image.tmdb.org/t/p/",
+  imageSize500: "w500",
+  imageSizeOriginal: "original",
+  youtubeBaseUrl: "https://www.youtube.com/watch?v=",
+  imdbBaseUrl: "https://www.imdb.com/title/",
+  readAccessToken:
+    "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlYTk2YTBkZDEwNDVkN2FkM2ZiYjcwOWQ3NTEzMDZiMiIsInN1YiI6IjY1OWEzYzkyMjE2MjFkMDIwMjM1NmFkMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.W3wpWYsYFpjVnJcAuMTNCHd_yS-EenNouJBm_y79DQM",
+};
+
+export default statikVariables;
